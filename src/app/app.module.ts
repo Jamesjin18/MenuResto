15d25a245b27a51b96c226651fafe9e6363ddd1a
James <james.jin@efrei.net>
2021-05-28 21:51:17 +0200

    add component module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { OrderComponent } from './order/order.component';
+import { SwitchComponent } from './switch/switch.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    OrderComponent,
+    SwitchComponent
   ],
   imports: [
     BrowserModule,
